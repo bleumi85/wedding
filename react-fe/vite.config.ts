a97868610cb88305793803048f95cbcc9e4c3a28
Jens Bleumer <jens.bleumer@gmail.com>
2024-01-16 15:06:54 +0100

    [react-fe] prettier fixes

--- react-fe/vite.config.ts
+++ react-fe/vite.config.ts
@@ -5,5 +5,5 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  build: { chunkSizeWarningLimit: 1600, }
+  build: { chunkSizeWarningLimit: 1600 },
 });
